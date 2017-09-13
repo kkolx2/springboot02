5f324e0b0ab6dd30c765b09f5165e98218ea2ef3
java@guestBig <kkolx2@nate.com>
2017-09-13 12:29:25 +0900

    3rd Commit

--- src/test/java/com/example/repository/DeptRepositoryTest.java
+++ src/test/java/com/example/repository/DeptRepositoryTest.java
@@ -3,12 +3,15 @@ package com.example.repository;
 import static org.junit.Assert.*;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
 import javax.inject.Inject;
 
 import static org.hamcrest.CoreMatchers.*;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,10 +23,10 @@ import com.example.domain.Dept;
 
 
 @RunWith(SpringRunner.class)				// JUnit Annotation
-//@SpringBootTest
+@SpringBootTest
 //@ActiveProfiles("oracle")					// Main Application 기동 옵션 중 프로필 사용 시 필요.
-//@ActiveProfiles("mysql")					// Main Application 기동 옵션 중 프로필 사용 시 필요.
-@DataJpaTest									// In Memory DataBase 필요. pom.xml
+@ActiveProfiles("mysql-ec2")					// Main Application 기동 옵션 중 프로필 사용 시 필요.
+//@DataJpaTest									// In Memory DataBase 필요. pom.xml (가상 DB)
 public class DeptRepositoryTest {
 
 	@Inject
@@ -40,4 +43,74 @@ public class DeptRepositoryTest {
 		assertEquals(new Dept(new BigInteger("10"),"경리예쁘다","서울XXX"),deptRepository.findOne(new BigInteger("10")));
 	}
 
+	@Test
+	public void insert(){
+		System.out.println(deptRepository.getClass());
+		deptRepository.save(new Dept(new BigInteger("10"),"경리부","서울"));
+		
+		System.out.println(deptRepository.findOne(new BigInteger("10")));
+		
+		assertEquals(new Dept(new BigInteger("10"),"경리부","서울"),deptRepository.findOne(new BigInteger("10")));
+	}
+	
+	@Test
+	public void update(){
+		System.out.println(deptRepository.getClass());
+		deptRepository.save(new Dept(new BigInteger("10"),"경리부","서울"));
+		deptRepository.save(new Dept(new BigInteger("10"),"경리예쁘다","서울XXX"));
+		
+		
+		System.out.println(deptRepository.findOne(new BigInteger("10")));
+		
+		assertEquals(new Dept(new BigInteger("10"),"경리예쁘다","서울XXX"),deptRepository.findOne(new BigInteger("10")));
+	}
+	
+	@Test
+	public void delete(){
+		System.out.println(deptRepository.getClass());
+		deptRepository.save(new Dept(new BigInteger("10"),"경리부","서울"));
+		deptRepository.save(new Dept(new BigInteger("10"),"경리예쁘다","서울XXX"));
+		deptRepository.delete(new BigInteger("10"));
+		
+		System.out.println(deptRepository.findOne(new BigInteger("10")));
+		
+		assertNull(deptRepository.findOne(new BigInteger("10")));
+	}
+	
+	@Test
+	public void selectAll(){
+		System.out.println(deptRepository.getClass());
+		Dept d10 = new Dept(new BigInteger("10"),"경리부","서울");
+		Dept d20 = new Dept(new BigInteger("20"),"인사부","인천");
+		Dept d30 = new Dept(new BigInteger("30"),"영업부","용인");
+		Dept d40 = new Dept(new BigInteger("40"),"전산부","수원");
+		
+		deptRepository.save(d10);
+		deptRepository.save(d20);
+		deptRepository.save(d30);
+		deptRepository.save(d40);
+		
+		deptRepository.findAll().forEach(e -> {
+			System.out.println(e);
+		});
+	}
+	
+	@Test
+	public void selectAll2(){
+		System.out.println(deptRepository.getClass());
+		Dept d10 = new Dept(new BigInteger("10"),"경리부","서울");
+		Dept d20 = new Dept(new BigInteger("20"),"인사부","인천");
+		Dept d30 = new Dept(new BigInteger("30"),"영업부","용인");
+		Dept d40 = new Dept(new BigInteger("40"),"전산부","수원");
+		List<Dept> list = Arrays.asList(d10,d20,d30,d40);
+		
+		deptRepository.save(list);
+
+		assertThat(deptRepository.findAll(),is(list));
+		
+		deptRepository.findAll().forEach(e -> {
+			System.out.println(e);
+		});
+	}
+	
 }
